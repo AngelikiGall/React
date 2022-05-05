92f66b2773d15ce0b87ca168352154fef8f21f84
TSIROA <TSIROA@PFIZER.COM>
2022-05-05 13:02:11 +0300

    Add table

--- src/pages/dashboard/dashboard.js
+++ src/pages/dashboard/dashboard.js
@@ -1,5 +1,6 @@
 import { useQuery } from "react-query";
 import { getMovies } from "../../api/movies";
+import { Table } from 'reactstrap';
 
 const Dashboard = () => {
   const { status, data: movies } = useQuery("movies", () => getMovies());
@@ -7,11 +8,33 @@ const Dashboard = () => {
   return (
     <div>
       <h1>Dashboard</h1>
-      <ul>
-        {movies?.map((movie) => (
-          <li key={movie.id}>{movie.title}</li>
-        ))}
-      </ul>
+
+      <div>Movies</div>
+      <h4>{movies?.length}</h4>
+      Last 5 movies
+      <Table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Release date</th>
+            <th>Runtime</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {movies?.map((movie) => (
+            <tr>
+              <td>{movie.id}</td>
+              <td>{movie.title}</td>
+              <td>{movie.release_date}</td>
+              <td>{movie.runtime}</td>
+              <td><a href={`/movie/{movied.id}`}>View Link</a></td>
+            </tr>
+            
+          ))}
+        </tbody>
+      </Table>
     </div>
   );
 };
