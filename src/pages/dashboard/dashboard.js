b0b877425ae66020df8f31273414ad35c4fe4ef0
Angeliki Galliou <angeliki.galliou@pfizer.com>
2022-05-05 12:19:03 +0300

    printed all movie titles.

--- src/pages/dashboard/dashboard.js
+++ src/pages/dashboard/dashboard.js
@@ -1,11 +1,38 @@
+import { useEffect } from "react";
+import {getMovies} from "../../api/movies";
+import { useState } from "react";
+
 function Dashboard() {
+    
+    const [movies, setMovies] = useState([]);
+    
+    useEffect( () => { 
+        getMovies().then( (movies) => {
+        //console.log(movies);
+        setMovies(movies);
+    });
+}, []) //to run one time only n mount. 
+
+function printMovieTitle(movie){
+    return movie.original_title;
+}
     return (
-        <div classname= "Dashboard">
+        <div className= "Dashboard">
             <h1> Dashboard </h1>
-            <label>Movies</label>
-
+            <div id="TotalMovies">
+                <div><label>Movies</label></div>
+                <div><label>{movies.length}</label></div>
+            </div>
+           
+            <div id="MovieList">
+                <ul>
+                    {movies?.map((movie) => <li>{movie.title}</li>)};
+                </ul>
+            </div>
+        
         </div>
-
+        
+        
     );
 }
 export default Dashboard;
