37b6105dddaee5320f5abb46c92f6d61155078be
TSIROA <TSIROA@PFIZER.COM>
2022-05-05 12:12:45 +0300

    Add header

--- src/index.js
+++ src/index.js
@@ -6,38 +6,18 @@ import reportWebVitals from './reportWebVitals';
 import Home from './pages/home/home';
 import Error404 from './pages/errror404';
 import Dashboard from './pages/dashboard/dashboard';
-import {BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+import {BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import {
-  Navbar,
-  NavbarBrand,
-  Collapse,
-  Nav,
-  NavItem,
-  NavLink,
-} from "reactstrap";
+import Header from './components/Header/Header';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <Navbar color="light" expand="md" light>
-        <NavbarBrand tag={Link} to="/">
-          React Tech Jam
-        </NavbarBrand>
-        <Collapse navbar>
-          <Nav className="me-auto" navbar>
-            <NavItem>
-              <NavLink tag={Link} to="/dashboard">
-                Dashboard
-              </NavLink>
-            </NavItem>
-          </Nav>
-        </Collapse>
-      </Navbar>
+    <Header></Header>
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/dashboard" element={<Dashboard />}/>
